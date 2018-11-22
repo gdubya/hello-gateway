22f3decfcc0d0ae4d849090a8ce3a491972abf5b
Gareth Western <gareth.western@bouvet.no>
2018-11-22 20:25:06 +0100

    list services

--- src/main/java/com/garethwestern/test/hellogateway/HelloGatewayApplication.java
+++ src/main/java/com/garethwestern/test/hellogateway/HelloGatewayApplication.java
@@ -1,8 +1,17 @@
 package com.garethwestern.test.hellogateway;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -11,4 +20,22 @@ public class HelloGatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HelloGatewayApplication.class, args);
 	}
+
+	@RestController
+	class ServiceInstanceRestController {
+
+		@Autowired
+		private DiscoveryClient discoveryClient;
+
+		@RequestMapping("/services/{applicationName}")
+		public List<ServiceInstance> serviceInstancesByApplicationName(@PathVariable String applicationName) {
+			return this.discoveryClient.getInstances(applicationName);
+		}
+
+		@RequestMapping("/services")
+		public List<String> allInstances() {
+			return this.discoveryClient.getServices();
+		}
+	}
+
 }
